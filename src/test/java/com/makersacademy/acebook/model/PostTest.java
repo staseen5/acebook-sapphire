cb7ba6b2f1f61a879a1969e7e1794ea517d8a7de
Samin <samin.taseen@outlook.com>
2026-03-10 13:16:24 +0000

    fix: PostTest unit test uses two args

--- src/test/java/com/makersacademy/acebook/model/PostTest.java
+++ src/test/java/com/makersacademy/acebook/model/PostTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class PostTest {
 
-	private Post post = new Post("hello");
+	private Post post = new Post("hello", 1L);
 
 	@Test
 	public void postHasContent() {
