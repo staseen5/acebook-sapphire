5f09daa9e7bca0f47eb67dd56ba079c1d7a705a9
David Jackson <davidboycjackson@gmail.com>
2026-03-11 22:09:48 +0000

    updated post and profile page templates to use user profile picture url

--- src/main/java/com/makersacademy/acebook/model/User.java
+++ src/main/java/com/makersacademy/acebook/model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Objects;
 
 import static java.lang.Boolean.TRUE;
 
@@ -39,5 +40,9 @@ public class User {
     public User(String email, boolean enabled) {
         this.email = email;
         this.enabled = enabled;
+    }
+
+    public String getProfilePictureUrl() {
+        return Objects.requireNonNullElse(this.profilePictureUrl, "https://static.vecteezy.com/system/resources/thumbnails/036/594/092/small/man-empty-avatar-photo-placeholder-for-social-networks-resumes-forums-and-dating-sites-male-and-female-no-photo-images-for-unfilled-user-profile-free-vector.jpg");
     }
 }
