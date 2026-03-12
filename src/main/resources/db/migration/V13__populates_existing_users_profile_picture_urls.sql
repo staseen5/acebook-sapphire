1dee30fcc8d95e1a43b74e78811337717753ad35
David Jackson <davidboycjackson@gmail.com>
2026-03-12 12:16:01 +0000

    Created migration for seed user profile pictures

---
+++ src/main/resources/db/migration/V13__populates_existing_users_profile_picture_urls.sql
@@ -0,0 +1,3 @@
+UPDATE users SET profile_picture_url = 'https://res.cloudinary.com/acebook-sapphire/image/upload/v1773316945/alice_headshot_ovvzio.jpg' WHERE email='alice';
+UPDATE users SET profile_picture_url = 'https://res.cloudinary.com/acebook-sapphire/image/upload/v1773317067/bob_headshot_dwlllm.jpg' WHERE email='bob';
+UPDATE users SET profile_picture_url = 'https://res.cloudinary.com/acebook-sapphire/image/upload/v1773317139/Charlie_Chaplin_hs1jlp.jpg' WHERE email='charlie';
