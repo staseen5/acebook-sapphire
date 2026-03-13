2c4f6019296c0252ebd24e4a2242c732c25f3a52
Samin <samin.taseen@outlook.com>
2026-03-13 14:56:39 +0000

    fix: tagged posts not appearing on profiles

--- src/main/java/com/makersacademy/acebook/repository/PostTagRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostTagRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface PostTagRepository extends CrudRepository<PostTag, PostTagId> {
     List<PostTag> findByIdUserId(Long userId);
+    List<PostTag> findByIdPostId(Long postId);
 
     boolean existsByIdPostIdAndIdUserId(Long postId, Long userId);
 }
